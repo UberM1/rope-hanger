e257b3c02bd3338407712ace5b3725de92d5e3e6
mubr <matiasuberti@letsbit.io>
2025-11-04 21:42:00 -0300

    wip: lazy_rope

--- lazy_rope.h
+++ lazy_rope.h
@@ -15,14 +15,40 @@ concept LazyUpdate = requires {
 template <typename Op>
   requires LazyUpdate<Op>
 class LazyRope {
+private:
+  void lazy_update(int l, int r, typename Op::Update x, int node, int node_l, int node_r) {
+    // Implementación de lazy_update
+  }
+
+  // lazy_query:
+
+
  public:
-  LazyRope(int n) { /* COMPLETAR */ }
-  typename Op::Value query(int l, int r) { /* COMPLETAR */ }
-  void update(int l, int r, typename Op::Update x) { /* COMPLETAR */ }
+  LazyRope(int n) {
+    int tree_size = 1;
+    while (tree_size < n) {
+      tree_size <<= 1;
+    }
+    data.resize(2 * tree_size - 1, Op::neut());
+    lazy_data.resize(2 * tree_size - 1, Op::neut());
+    this->n = tree_size;
+  }
+
+  typename Op::Value query(int l, int r) {
+    return lazy_query(l, r, 0, 0, n);
+  }
+
+  void update(int l, int r, typename Op::Update x) {
+    lazy_update(l, r, x, 0, 0, n);
+  }
+
+  void lazy_update(int l, int r, typename Op::Update x) {
+    local_lazy_update(l, r, x, 0, 0, n);
+  }
 
  private:
   std::vector<typename Op::Value> data;
-  std::vector<typename Op::Update> lazy;
+  std::vector<typename Op::Update> lazy_data;
 };
 
 #endif // LAZY_ROPE_H
